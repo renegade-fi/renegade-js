01df76d5668bff67e08bc346cae857930fb4723d
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2023-11-01 12:12:23 -0700

    Fix bug preventing new order placement

--- dist/account.js
+++ dist/account.js
@@ -358,7 +358,7 @@ export default class Account {
             return acc;
         }, {});
         const zeroOrders = findZeroOrders(orders);
-        if (Object.keys(this.orders).length < 5) {
+        if (Object.keys(orders).length < 5) {
             return await this.placeOrder(order);
         }
         else if (zeroOrders.length > 0) {
