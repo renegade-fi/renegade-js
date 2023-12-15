bfaefeb4b737fa5fb30b7d1e6530e2c7f651147d
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2023-12-14 18:45:17 -0800

    Commit wasm module

---
+++ dist/poseidon2/index.d.ts
@@ -0,0 +1,40 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* Computes the Poseidon2 hash of the input string and returns a BigInt.
+*
+* Note: Ensure the input is within the field of the BN254 curve and is a BigInt formatted as a hex string.
+* @param {string} value
+* @returns {bigint}
+*/
+export function compute_poseidon_hash(value: string): bigint;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly compute_poseidon_hash: (a: number, b: number) => number;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
--- poseidon2/build.sh
+++ poseidon2/build.sh
@@ -9,3 +9,6 @@ wasm-pack build --target web --out-dir $OUTPUT_DIR --out-name $OUTPUT_NAME
 # Optimize the WebAssembly binary
 wasm-opt -Oz -o $OUTPUT_DIR/${OUTPUT_NAME}_bg.wasm $OUTPUT_DIR/${OUTPUT_NAME}_bg.wasm
 
+# Delete the .gitignore file so the package is included
+rm $OUTPUT_DIR/.gitignore
+
