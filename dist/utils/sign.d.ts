0a94e0593671ac53b329f37af9b77162e3f2b76a
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2023-12-20 14:16:43 -0800

    Add valid wallet update statement signature

---
+++ dist/utils/sign.d.ts
@@ -0,0 +1,43 @@
+import { Order, Token, Wallet } from "../state";
+import { OrderId } from "../types";
+/**
+ * Sign the shares of a wallet after performing a deposit.
+ *
+ * @param wallet The wallet to sign the shares for.
+ * @param mint The Token to deposit.
+ * @param amount The amount to deposit.
+ */
+export declare function signWalletDeposit(wallet: Wallet, mint: Token, amount: bigint): string;
+/**
+ * Sign a wallet withdrawal.
+ *
+ * @param wallet The wallet to sign the shares for.
+ * @param mint The Token to withdraw.
+ * @param amount The amount to withdraw.
+ */
+export declare function signWalletWithdraw(wallet: Wallet, mint: Token, amount: bigint): any;
+/**
+ * Sign wallet to place an order.
+ *
+ * @param wallet The wallet to sign the shares for.
+ * @param order The order to place.
+ *
+ * Assumes this function is called after verifying wallet orderbook has space.
+ */
+export declare function signWalletPlaceOrder(wallet: Wallet, order: Order): any;
+/**
+ * Sign wallet to modify an order.
+ *
+ * @param wallet The wallet to sign the shares for.
+ * @param oldOrderId The ID of the order to modify.
+ * @param newOrder The new order to replace the old order.
+ *
+ */
+export declare function signWalletModifyOrder(wallet: Wallet, oldOrderId: OrderId, newOrder: Order): any;
+/**
+ * Sign wallet to cancel an order.
+ *
+ * @param wallet The wallet to sign the shares for.
+ * @param orderId The ID of the order to cancel.
+ */
+export declare function signWalletCancelOrder(wallet: Wallet, orderId: OrderId): any;
---
+++ dist/utils/sign.js
@@ -0,0 +1,119 @@
+import { Balance, Wallet } from "../state";
+import { bigIntToUint8Array } from "../state/utils";
+/**
+ * Sign the shares of a wallet.
+ *
+ * @param wallet The Wallet to sign the shares for.
+ */
+function signWalletShares(wallet) {
+    // TODO: If this doesn't work, change wallet serialization encoding method.
+    const serializedWallet = wallet.serialize();
+    // TODO: Should only sign blinded? public shares
+    const walletSignatureHex = wallet.keychain.keyHierarchy.root.signMessage(serializedWallet);
+    console.log("🚀 ~ signWalletShares ~ walletSignatureHex:", walletSignatureHex);
+    const walletSignatureBytes = bigIntToUint8Array(BigInt("0x" + walletSignatureHex));
+    // TODO: Should return bytes not string
+    return `[${walletSignatureBytes}]`;
+}
+/**
+ * Sign the shares of a wallet after performing a deposit.
+ *
+ * @param wallet The wallet to sign the shares for.
+ * @param mint The Token to deposit.
+ * @param amount The amount to deposit.
+ */
+export function signWalletDeposit(wallet, mint, amount) {
+    try {
+        const newBalances = [...wallet.balances];
+        const index = newBalances.findIndex((balance) => balance.mint === mint);
+        if (index === -1) {
+            newBalances.push(new Balance({ mint, amount }));
+        }
+        else {
+            newBalances[index] = new Balance({
+                mint,
+                amount: newBalances[index].amount + amount,
+            });
+        }
+        const newWallet = new Wallet({
+            ...wallet,
+            balances: newBalances,
+        });
+        return signWalletShares(newWallet);
+    }
+    catch (error) {
+        console.error("Error signing wallet update: ", error);
+    }
+}
+/**
+ * Sign a wallet withdrawal.
+ *
+ * @param wallet The wallet to sign the shares for.
+ * @param mint The Token to withdraw.
+ * @param amount The amount to withdraw.
+ */
+export function signWalletWithdraw(wallet, mint, amount) {
+    const newBalances = [...wallet.balances];
+    const index = newBalances.findIndex((balance) => balance.mint === mint);
+    const newBalance = newBalances[index].amount - amount;
+    newBalances[index] = new Balance({
+        mint,
+        amount: newBalance,
+    });
+    const newWallet = new Wallet({
+        ...wallet,
+        balances: newBalances,
+    });
+    return this.signWalletShares(newWallet);
+}
+/**
+ * Sign wallet to place an order.
+ *
+ * @param wallet The wallet to sign the shares for.
+ * @param order The order to place.
+ *
+ * Assumes this function is called after verifying wallet orderbook has space.
+ */
+export function signWalletPlaceOrder(wallet, order) {
+    const newOrders = [...wallet.orders].concat(order);
+    const newWallet = new Wallet({
+        ...wallet,
+        orders: newOrders,
+    });
+    return this.signWalletShares(newWallet);
+}
+/**
+ * Sign wallet to modify an order.
+ *
+ * @param wallet The wallet to sign the shares for.
+ * @param oldOrderId The ID of the order to modify.
+ * @param newOrder The new order to replace the old order.
+ *
+ */
+export function signWalletModifyOrder(wallet, oldOrderId, newOrder) {
+    const newOrders = [...wallet.orders];
+    const index = newOrders.findIndex((order) => order.orderId === oldOrderId);
+    newOrders[index] = newOrder;
+    const newWallet = new Wallet({
+        ...wallet,
+        orders: newOrders,
+    });
+    return this.signWalletShares(newWallet);
+}
+// TODO Verify this is same behavrior as relayer.
+/**
+ * Sign wallet to cancel an order.
+ *
+ * @param wallet The wallet to sign the shares for.
+ * @param orderId The ID of the order to cancel.
+ */
+export function signWalletCancelOrder(wallet, orderId) {
+    const newOrders = [...wallet.orders];
+    const index = newOrders.findIndex((order) => order.orderId === orderId);
+    newOrders.splice(index, 1);
+    const newWallet = new Wallet({
+        ...wallet,
+        orders: newOrders,
+    });
+    return this.signWalletShares(newWallet);
+}
